42d4857ba53b5f8bee417bfc0348e3ef4df533db
amiminn <tholabul.amin@gmail.com>
2024-05-16 15:27:53 +0700

    update favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <title>{{ env('APP_NAME')}}</title>
-    
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>😏</text></svg>"
+    />
     <script>
       window.Laravel = {csrfToken: '{{ csrf_token() }}',app_key : '{{ env("APP_KEY") }}',url : '{{ env("APP_URL") }}'}
       @auth
